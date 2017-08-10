e9c6b854c81870dd8fc3dc02f59de56da14355a7
naseef012 <naseefmohammed012@gmail.com>
2017-08-11 00:43:23 +0600

    Implemented the Breadth First Search Method

--- src/GraphImplementation.java
+++ src/GraphImplementation.java
@@ -1,4 +1,6 @@
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * Created by USER on 8/8/2017.
@@ -24,7 +26,32 @@ public class GraphImplementation
     }
 
 
+    public void BFS (int s)
+    {
+        boolean visited [] = new boolean[vertices];
 
+        visited[s] = true;
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.add(s);
+        //ListIterator i = graph[s].listIterator();
+
+        while (queue.size()!=0)
+        {
+            s = queue.poll();
+            System.out.print (s+ " ");
+            Iterator<Integer> i = graph[s].listIterator();
+            while (!i.hasNext())
+            {
+                int n = i.next();
+                if (!visited[n])
+                {
+                    queue.add(n);
+                    visited[n] = true;
+                }
+            }
+        }
+
+    }
 
 
 
