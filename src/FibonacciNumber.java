62d7a6124b9de097473bd55d7fc353b119be7867
naseef012 <naseefmohammed012@gmail.com>
2017-08-18 00:58:06 +0600

    Implemented fibonacci number sequence

---
+++ src/FibonacciNumber.java
@@ -0,0 +1,17 @@
+/**
+ * Created by USER on 8/15/2017.
+ */
+public class FibonacciNumber
+{
+    public static long fibonacci(int n) {
+        if (n <= 1) return n;
+        else return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+    public static void main(String[] args)
+    {
+        int n = 4;
+        for (int i = 1; i <= n; i++)
+            System.out.println(i + ": " + fibonacci(i));
+    }
+}
