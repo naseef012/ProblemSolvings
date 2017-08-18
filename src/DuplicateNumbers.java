75601cc574c418054bceaac65b998577f7c44271
naseef012 <naseefmohammed012@gmail.com>
2017-08-18 18:01:03 +0600

    Implemented using HashMap and made the program perfectly find the duplicates

--- src/DuplicateNumbers.java
+++ src/DuplicateNumbers.java
@@ -1,47 +1,59 @@
 import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Created by USER on 8/16/2017.
+ *
+ * Problem: Find the duplicate numbers in a given array
+ */
+
+/**
+ * This is implemented using HashMap.
+ * Time Complexity is O(2n)
+ * Space Complexity is O(n)
  */
 public class DuplicateNumbers
 {
     public void findDuplicate (int [] arr)
     {
+        /*HashSet<Integer> set = new HashSet<>();
         Arrays.sort(arr);
-        /*for (int k =0;k<arr.length;k++)
+        for (int s: arr)
         {
-            System.out.println(arr[k]);
-        }*/
-        int j =0;
-        int temp [] = new int[arr.length];
-        for(int i=0;i<arr.length-1;i++)
-        {
-            System.out.println(j);
-
-                if (temp[j]==arr[i])
-                {
-                    System.out.println("hi");
-                    break;
-                }
-
-            else if (arr[i] == arr[i+1])
+            if(!set.add(s))
             {
-                temp[j] = arr[i];
-                j++;
+                System.out.println(s);
+            }
+        }*/
+        HashMap<Integer , Integer> map = new HashMap<>();
+
+        for (int s: arr)
+        {
+            Integer count = map.get(s);
+            if(count==null)
+            {
+                map.put(s,1);
+            }
+            else
+            {
+                map.put(s,++count);
             }
         }
 
-        for (int k =0;k<j;k++)
-        {
-            System.out.println("lol " + temp[k]);
-        }
-    }
+       for (Map.Entry m : map.entrySet())
+       {
+           int c =(int) m.getValue();
+           if (c>1)
+           {
+               System.out.println("duplicate "+ m.getKey());
+           }
 
+       }
+    }
     public static void main(String[] args)
     {
         DuplicateNumbers dpl = new DuplicateNumbers();
-        int arr[] = {1,2,1,3,3,3,3,4,5,5,5};
+        int arr[] = {1,3,7,7,19,3,3,2,1,3,4,5,5,5};
         dpl.findDuplicate(arr);
     }
 }
