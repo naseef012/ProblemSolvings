d0c84ffc7f4776d4eb6cd4e714884b9c22b05b60
naseef012 <naseefmohammed012@gmail.com>
2017-09-14 15:58:28 +0600

    Implemented Fibonacci Number

--- src/FibonacciNumber.java
+++ src/FibonacciNumber.java
@@ -10,7 +10,7 @@ public class FibonacciNumber
 
     public static void main(String[] args)
     {
-        int n = 4;
+        int n = 10;
         for (int i = 1; i <= n; i++)
             System.out.println(i + ": " + fibonacci(i));
     }
