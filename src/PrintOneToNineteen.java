3bd22147bb564adf73880bc9a9f627afed623783
naseef012 <naseefmohammed012@gmail.com>
2017-09-21 18:49:00 +0600

    Implemented Print 1 2 3 4 ....4 3 2 1 in one loop

---
+++ src/PrintOneToNineteen.java
@@ -0,0 +1,23 @@
+/**
+ * Created by USER on 9/21/2017.
+ *
+ * Problem: Print
+ *
+ *          "1 2 3 4 5 6 7 8 9 10 9 8 7 6 5 4 3 2 1"
+ *
+ *          in one loop.
+ */
+public class PrintOneToNineteen
+{
+    public static void main(String[] args)
+    {
+        for (int i=1;i<20;i++)
+        {
+            if (i<=10)
+                System.out.println(i);
+            else
+                System.out.println(20-i);
+        }
+    }
+}
+//
