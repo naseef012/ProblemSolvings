56656d59f33838e246342fec22c359009252b524
naseef012 <naseefmohammed012@gmail.com>
2017-10-12 14:08:33 +0600

    Implemented Merge Sort Algorithim and solved kth smallest or largest element

---
+++ src/KthSmallestOrLargestElement.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created By Naseef M Abdus Sattar on 10/11/2017 at 11:48 PM
+ *
+ * Given an array and a number k where k is smaller than size of array, we need to find the k’th smallest element in the given array.
+ * It is given that all array elements are distinct.
+ *
+ * Input: arr[] = {7, 10, 4, 3, 20, 15}
+        k = 3
+ * Output: 7
+ */
+
+public class KthSmallestOrLargestElement
+{
+
+    public void findTheElement(int [] arr , int index)
+    {
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println("The "+index+"th smallest number is "+arr[index-1]);
+        System.out.println("The "+index+"th largest number is "+arr[(arr.length-1) - (index-1)]);
+    }
+
+    public static void main(String[] args)
+    {
+        int n;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter Array Size : ");
+        n = s.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the array elements: ");
+        for (int i=0;i<arr.length;i++)
+        {
+            int j = s.nextInt();
+            arr[i] = j;
+        }
+
+        System.out.println("Enter the index which you want to find: ");
+        int kthPosition =s.nextInt();
+        KthSmallestOrLargestElement func = new KthSmallestOrLargestElement();
+        func.findTheElement(arr,kthPosition);
+    }
+}
+/*
+
+ */
---
+++ src/MergeSortAlgo.java
@@ -0,0 +1,78 @@
+/**
+ * Created By Naseef M Abdus Sattar on 10/2/2017 at 9:23 PM
+ */
+public class MergeSortAlgo
+{
+    private int [] mainArray;
+
+    public void Merge (int[] left, int[] right, int[] mainArray)
+    {
+        int lengthOfLeftArray = left.length;
+        int lengthOfRightArray = right.length;
+
+        // iterator counts for Left array, Right array and Main array respectively
+        int i=0,j=0,k=0;
+
+        /*
+            This loop checks which subarray's element is smaller
+            which is then overwritten in the main array.
+         */
+        while (i < lengthOfLeftArray && j < lengthOfRightArray)
+        {
+            if (left[i] < right[j])
+            {
+                mainArray[k]=left[i];
+                i++;
+            }
+            else
+            {
+                mainArray[k]=right[j];
+                j++;
+            }
+            k++;
+        }
+
+        /*
+            This loop will execute if the two subarrays are not equal in length and any one
+            is exhausted before the other.
+         */
+        while (i<lengthOfLeftArray || j<lengthOfRightArray)
+        {
+            if (i<lengthOfLeftArray)
+            {
+                mainArray[k]=left[i];
+                i++;
+            }
+            else if (j<lengthOfRightArray)
+            {
+                mainArray[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+    }
+
+    public void MergeSort(int[] array)
+    {
+        int lengthOfArray = array.length;
+        if (lengthOfArray < 2)
+            return;
+        int midPoint = lengthOfArray/2;
+        int [] left = new int[midPoint];
+        int [] right = new int[lengthOfArray - midPoint];
+
+        for (int i=0; i<midPoint-1;i++)
+        {
+            left[i] = array[i];
+        }
+        for (int i=midPoint;i<lengthOfArray-1;i++)
+        {
+            right[i-midPoint] = array[i];
+        }
+        MergeSort(left);
+        MergeSort(right);
+        Merge(left,right,array);
+
+    }
+}
